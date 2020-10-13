6016aed76cf19ddc89dbc8800147da4c2f958836
18784097076 <1711002115@qq.com>
2020-10-13 11:07:01 +0800

    createComponents功能分支已经完成了对静态组件的拆分

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="main">
+        <HeaderTop title="我的"/>
+
+        <div id="content">
+             <Login/>
+        </div>
+
+        <TabBar/>
+    </div>
+</template>
+
+<script>
+import HeaderTop from "@/components/HeaderTop";
+import TabBar from "@/components/TabBar";
+import Login from '@/components/Login'
+export default {
+    name: 'Mine',
+    components: {
+        HeaderTop,
+        TabBar,
+        Login
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
